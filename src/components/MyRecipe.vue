8f0faae35e02eca16e2c955b0a9cb1ddbedb98c2
yemson <lier@kakao.com>
2022-10-14 03:09:23 +0900

    feat: 나의 레피시 카드 추가

---
+++ src/components/MyRecipe.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container mt-4">
+    <div class="fs-4 fw-bold text-success text-start">
+      나의 레시피
+    </div>
+    <hr class="mt-2">
+    <!-- NOTE: 일단 카드 반응형인데 width 부분 auto로 하면 크기가 들쭉 날쭉 함 -->
+    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 text-start">
+      <div class="col mb-4">
+        <router-link
+          to="/create-recipe"
+          style="text-decoration: none; color: inherit"
+        >
+          <div
+            class="card shadow-sm"
+            style="width: auto; height: 26rem;"
+          >
+            <div class="card-body d-flex">
+              <p class="card-text text-center align-self-center fw-bold fs-5">
+                로그인하여 나만의 레시피를 만들어보세요!
+              </p>
+            </div>
+          </div>
+        </router-link>
+      </div>
+      <div
+        v-for="(recipe, index) in recipes"
+        :key="index"
+      >
+        <!-- TODO: router-link 씌우고 레시피 디테일 페이지 연결해야 함 -->
+        <div class="col mb-4">
+          <div
+            class="card shadow-sm"
+            style="width: auto; height: 26rem;"
+          >
+            <img
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+              class="card-img-top"
+              height="200"
+            >
+            <div class="card-body">
+              <h5 class="card-title">
+                {{ recipe.recipeName }}
+              </h5>
+              <p class="card-text">
+                {{ recipe.recipeDesc }}
+              </p>
+            </div>
+            <div class="card-footer text-muted fs-6">
+              {{ recipe.uid }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onAuthStateChanged } from 'firebase/auth'
+import { query, collection, onSnapshot, where } from 'firebase/firestore'
+import { auth, db } from '@/main'
+
+export default {
+  name: 'MyRecipe',
+  components: {
+  },
+  data () {
+    return {
+      user: null,
+      recipes: []
+    }
+  },
+  mounted () {
+    this.checkAuth()
+    this.getMyRecipes()
+  },
+  methods: {
+    getMyRecipes () {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const q = query(collection(db, 'recipe_post'), where('uid', '==', `${user.uid}`))
+          onSnapshot(q, (snapshot) => {
+            this.recipes = []
+            snapshot.forEach((doc) => {
+              this.recipes.push({
+                id: doc.id,
+                ...doc.data()
+              })
+            })
+          })
+        }
+      })
+    },
+    checkAuth () {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          this.user = user
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scope>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <Nav />
+    <!-- TODO: Carousel 추가 -->
+    <MyRecipe />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Nav from '@/components/Nav.vue'
+import MyRecipe from '../components/MyRecipe.vue'
 
 export default {
   name: 'Home',
   components: {
-    Nav
+    Nav,
+    MyRecipe
   },
   data () {
     return {
