0a45caeb67acccd799f63f950c75d5bf6d92897b
yemson <lier@kakao.com>
2022-10-19 20:16:24 +0900

    fix: 로그인 페이지 반응형 수정

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,74 +1,80 @@
 <template>
   <div>
     <Nav />
-    <div
-      class="card shadow-sm"
-      style="width: 30em; height: 28em; margin: 4em auto 0;"
-    >
-      <div
-        class="card-body"
-        style="text-align: left;"
-      >
+    <div class="row">
+      <div class="col col-sm-2 col-md-2 col-lg-3 col-xl-4 col-xxl-4" />
+      <div class="col-12 col-sm-8 col-md-8 col-lg-6 col-xl-4 col-xxl-4">
         <div
-          class="card-title; mx-3 mt-2 fw-bold"
-          style="font-size: 2.5em;"
+          class="card shadow-sm"
+          style="height: 28em; margin: 4em auto 0;"
         >
-          로그인
-        </div>
-        <div
-          class="mx-3"
-          style="margin-top: 2rem;"
-        >
-          <label
-            for="emailInput"
-            class="form-label fw-bold"
-          >이메일</label>
-          <input
-            id="emailInput"
-            v-model="email"
-            type="email"
-            class="form-control shadow-sm fw-bold"
-            placeholder="이메일 입력"
+          <div
+            class="card-body"
+            style="text-align: left;"
           >
-        </div>
-        <div
-          class="mx-3"
-          style="margin-top: 1.5rem;"
-        >
-          <label
-            for="passwordInput"
-            class="form-label fw-bold"
-          >비밀번호</label>
-          <input
-            id="passwordInput"
-            v-model="password"
-            type="password"
-            class="form-control shadow-sm fw-bold"
-            placeholder="비밀번호 입력"
-            @keyup.enter="login"
-          >
-        </div>
-        <div
-          class="d-grid gap-2 mx-3"
-          style="margin-top: 2.5rem;"
-        >
-          <button
-            class="btn btn-success mb-2 shadow-sm fw-bold"
-            type="button"
-            @click="login"
-          >
-            로그인
-          </button>
-          <!-- TODO: 회원가입 페이지 만들고 링크 -->
-          <!-- <router-link
-            class="btn btn-outline-success shadow-sm fw-bold"
-            type="button"
-            to="/register"
-          >
-            회원가입
-          </router-link> -->
+            <div
+              class="card-title; mx-3 mt-2 fw-bold"
+              style="font-size: 2.5em;"
+            >
+              로그인
+            </div>
+            <div
+              class="mx-3"
+              style="margin-top: 2rem;"
+            >
+              <label
+                for="emailInput"
+                class="form-label fw-bold"
+              >이메일</label>
+              <input
+                id="emailInput"
+                v-model="email"
+                type="email"
+                class="form-control shadow-sm fw-bold"
+                placeholder="이메일 입력"
+              >
+            </div>
+            <div
+              class="mx-3"
+              style="margin-top: 1.5rem;"
+            >
+              <label
+                for="passwordInput"
+                class="form-label fw-bold"
+              >비밀번호</label>
+              <input
+                id="passwordInput"
+                v-model="password"
+                type="password"
+                class="form-control shadow-sm fw-bold"
+                placeholder="비밀번호 입력"
+                @keyup.enter="login"
+              >
+            </div>
+            <div
+              class="d-grid gap-2 mx-3"
+              style="margin-top: 2.5rem;"
+            >
+              <button
+                class="btn btn-success mb-2 shadow-sm fw-bold"
+                type="button"
+                @click="login"
+              >
+                로그인
+              </button>
+              <!-- TODO: 회원가입 페이지 만들고 링크 -->
+              <!-- <router-link
+                class="btn btn-outline-success shadow-sm fw-bold"
+                type="button"
+                to="/register"
+              >
+                회원가입
+              </router-link> -->
+            </div>
+          </div>
         </div>
       </div>
+      <div class="col-12 col-sm-2 col-md-2 col-lg-6 col-xl-4 col-xxl-4" />
     </div>
   </div>
 </template>
